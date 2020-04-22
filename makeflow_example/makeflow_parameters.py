60f55e949a02103a5b83d5df63a5801cb6318f3c
fratim <frtim@student.ethz.ch>
2020-04-22 06:03:20 -0400

    code ran for 1024x1024x1024

--- makeflow_example/makeflow_parameters.py
+++ makeflow_example/makeflow_parameters.py
@@ -4,14 +4,16 @@ checkfile_folder    = "/n/pfister_lab2/Lab/tfranzmeyer/exp/output_files/check_fi
 stdout_folder       = "/n/pfister_lab2/Lab/tfranzmeyer/exp/output_files/stdout_files/Zebrafinch/"
 stderr_folder       = "/n/pfister_lab2/Lab/tfranzmeyer/exp/output_files/stderr_files/Zebrafinch/"
 
-RAM_HF_S1_S4  = 6000
-RAM_HF_S2     = 6000
-RAM_HF_S3     = 6000
-RAM_SK_S1_S2  = 6000
-RAM_SK_S3     = 6000
-RAM_SK_S4     = 6000
-RAM_ST_S1     = 6000
-RAM_ST_S2     = 6000
+blockconsumption = 1024*1024*1024*8/1000/1000
+
+RAM_HF_S1_S4  = int(blockconsumption*3.5)
+RAM_HF_S2     = int(blockconsumption*0.5)
+RAM_HF_S3     = int(30000)
+RAM_SK_S1_S2  = int(blockconsumption*1.5)
+RAM_SK_S3     = int(blockconsumption*2)
+RAM_SK_S4     = int(10000)
+RAM_ST_S1     = int(blockconsumption*2.7)
+RAM_ST_S2     = int(10000)
 
 cluster_partition = "test"
 job_time = "0-00:59"
