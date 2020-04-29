8a6717d38f4da47a74845651cfd1d453dddef0fc
Brian Matejek <bmatejek@gmail.com>
2020-04-28 23:24:40 -0400

    timing: added method for FIFO and LPT job scheduling

--- evaluate/timing.py
+++ evaluate/timing.py
@@ -254,173 +254,223 @@ def SkeletonRefinement(data):
 
 
 
-def LPT(jobs, m_cpus):
-    # create empty cpus
+# def LPT(jobs, m_cpus):
+#     # create empty cpus
+#     cpu_times = np.zeros(m_cpus, dtype=np.float32)
+#
+#     # sort the jobs in descending running time
+#     # do not use in-place sorting since times must remain sorted elsewhere
+#     sorted_jobs = sorted(jobs, reverse = True)
+#
+#     for job in sorted_jobs:
+#         estimated_best_cpu = np.argmin(cpu_times)
+#
+#         cpu_times[estimated_best_cpu] += job
+#
+#     clock_time = max(cpu_times)
+#     idle_time = (m_cpus * clock_time) - sum(cpu_times)
+#
+#     return clock_time, idle_time
+#
+#
+#
+# def PlotClockAndIdleTime(data, clock_times, idle_times, output_filename):
+#     fig, ax1 = plt.subplots()
+#
+#     max_cpus = len(clock_times) + 1
+#     cpus = range(1, max_cpus)
+#
+#     ax1.set_xlabel('No. CPUs')
+#     ax1.set_ylabel('Total Clock Time (s)')
+#     ax1.plot(cpus, clock_times)
+#
+#     ax1.set_xlim(1, max_cpus)
+#
+#     ax2 = ax1.twinx()
+#     ax2.set_ylabel('CPU Time Idle (%)')
+#     ax2.plot(cpus, idle_times, color='r')
+#
+#     # get the output filename for this block
+#     figures_directory = data.FiguresDirectory()
+#     if not os.path.exists(figures_directory):
+#         os.makedirs(figures_directory, exist_ok=True)
+#
+#     output_filename = '{}/{}'.format(figures_directory, output_filename)
+#
+#     # show the figure
+#     plt.savefig(output_filename)
+#
+#
+#
+# def ComputeParallelStatistics(data, times):
+#     # find the most cpus that could be useful (most jobs in one step)
+#     maximum_cpus = 0
+#     for step in times.keys():
+#         maximum_cpus = max(maximum_cpus, len(times[step]))
+#
+#     total_clock_times = []
+#     total_idle_times = []
+#
+#     # calculate the optimal amount of time with this many CPUs
+#     for cpu in range(1, maximum_cpus + 1):
+#
+#         # the clock time and idle time for this number of cpus
+#         total_clock_time = 0
+#         total_idle_time = 0
+#
+#         for step in times.keys():
+#             # estimate the optimal time using the Longest Processing Time (LPT) heuristic
+#             # achieves an upper bound of 4/3 - 1 / (3m) the optimal time
+#             lpt_clock_time, lpt_idle_time = LPT(times[step], cpu)
+#
+#             # update the total clock and idle time after this step
+#             total_clock_time += lpt_clock_time
+#             total_idle_time += lpt_idle_time
+#
+#         total_clock_times.append(total_clock_time)
+#         total_idle_times.append(total_idle_time)
+#
+#     PlotClockAndIdleTime(data, total_clock_times, total_idle_times, 'cpu-usage.png')
+#
+#
+#
+# def GenerateBlockTimingPlot(data, description, n_non_zero_voxels, times, output_filename):
+#     fig, ax = plt.subplots()
+#
+#     # set the title and labels
+#     ax.set_title('{} {}'.format(data.FigureTitleDescription(), description), fontsize=14)
+#     ax.set_ylabel('CPU Time (s)', fontsize=10)
+#     ax.set_xlabel('No. Non Zero Voxels (Millions)', fontsize=10)
+#
+#     # show a scatter plot of voxels and times
+#     ax.scatter(n_non_zero_voxels, times)
+#
+#     # generate a best fit line
+#     m, b = np.polyfit(n_non_zero_voxels, times, 1)
+#
+#     # create x and y chains for final plot
+#     x = np.linspace(min(n_non_zero_voxels), max(n_non_zero_voxels), num=500)
+#     y = m * x + b
+#     ax.plot(x, y, color='#ff7d74', label='{:0.2f} seconds / million'.format(m))
+#
+#     # draw the legend
+#     ax.legend()
+#
+#     fig.tight_layout()
+#
+#     # get the output filename for this block
+#     figures_directory = data.FiguresDirectory()
+#     if not os.path.exists(figures_directory):
+#         os.makedirs(figures_directory, exist_ok=True)
+#
+#     output_filename = '{}/{}'.format(figures_directory, output_filename)
+#
+#     # show the figure
+#     plt.savefig(output_filename)
+#
+#
+#
+# def ConductBlockTimingAnalysis(data, times):
+#     # directory that contains
+#     statistics_directory = '{}/statistics'.format(data.TempDirectory())
+#
+#     # create empty arrays for the block timing analysis
+#     component_times = []
+#     connect_labels_times = []
+#     fill_holes_times = []
+#     save_anchor_walls_times = []
+#     compute_anchor_points_times = []
+#     topological_thinning_times = []
+#
+#     # create aggregate variables
+#     hole_filling_times = []
+#     skeletonize_times = []
+#     total_times = []
+#
+#     n_non_zero_voxels = []
+#
+#     # iterate over every block
+#     index = 0 # counter to return the total time for each block
+#     for iz in range(data.StartZ(), data.EndZ()):
+#         for iy in range(data.StartY(), data.EndY()):
+#             for ix in range(data.StartX(), data.EndX()):
+#                 statistics_filename = '{}/{:04d}z-{:04d}y-{:04d}x.pickle'.format(statistics_directory, iz, iy, ix)
+#                 statistics = ReadPickledData(statistics_filename)
+#
+#                 # get the the number of non zero voxels in this block (in millions)
+#                 n_non_zero_voxels.append(statistics['filled_n_non_zero'] / million)
+#
+#                 # get the times for each step for skeleton generation
+#                 save_anchor_walls_times.append(times['save-anchor-walls'][index])
+#                 compute_anchor_points_times.append(times['compute-anchor-points'][index])
+#                 topological_thinning_times.append(times['topological-thinning'][index])
+#                 skeletonize_times.append(save_anchor_walls_times[-1] + compute_anchor_points_times[-1] + topological_thinning_times[-1])
+#
+#                 # get the time for each step for hole filling
+#                 if not data.HoleFillingOutputDirectory() == None:
+#                     component_times.append(times['components'][index])
+#                     connect_labels_times.append(times['connect-labels'][index])
+#                     fill_holes_times.append(times['fill-holes'][index])
+#                     hole_filling_times.append(component_times[-1] + connect_labels_times[-1] + fill_holes_times[-1])
+#                     total_times.append(hole_filling_times[-1] + skeletonize_times[-1])
+#
+#                 index += 1
+#
+#     GenerateBlockTimingPlot(data, 'Skeleton Generation Time', n_non_zero_voxels, skeletonize_times, 'skeleton-generation-time.png')
+#
+#     if not data.HoleFillingOutputDirectory() == None:
+#         GenerateBlockTimingPlot(data, 'Hole Filling Time', n_non_zero_voxels, hole_filling_times, 'hole-filling-time.png')
+#         GenerateBlockTimingPlot(data, 'Total Running Time', n_non_zero_voxels, total_times, 'total-running-time.png')
+
+
+
+def WallTime(data, times, m_cpus, strategy = 'FIFO'):
+    # create an array of empty cpus
     cpu_times = np.zeros(m_cpus, dtype=np.float32)
 
-    # sort the jobs in descending running time
-    # do not use in-place sorting since times must remain sorted elsewhere
-    sorted_jobs = sorted(jobs, reverse = True)
+    # these tasks must be completed (no variation of input in naive order)
+    if not data.HoleFillingOutputDirectory() == None:
+        tasks = ['components', 'connect-labels', 'combine-labels', 'fill-holes']
+    else:
+        tasks = []
+    tasks += ['save-anchor-walls', 'compute-anchor-points', 'topological-thinning', 'skeleton-refinement']
 
-    for job in sorted_jobs:
-        estimated_best_cpu = np.argmin(cpu_times)
+    # create the order of jobs in a naive FIFO fashion where the times are determined by block index
+    jobs = []
 
-        cpu_times[estimated_best_cpu] += job
+    # iterate over all tasks
+    for task in tasks:
+        # iterate over every time for this task (already in block index order)
+        if strategy == 'FIFO':
+            for time in times[task]:
+                jobs.append(time)
+        elif strategy == 'LPT':
+            for time in sorted(times[task], reverse=True):
+                jobs.append(time)
 
-    clock_time = max(cpu_times)
-    idle_time = (m_cpus * clock_time) - sum(cpu_times)
+    # each job is placed on the cpu with the lowest time
+    for job in jobs:
+        idle_cpu = np.argmin(cpu_times)
 
-    return clock_time, idle_time
+        # this job is now allocated to the first idle cpu
+        cpu_times[idle_cpu] += job
 
-
-
-def PlotClockAndIdleTime(data, clock_times, idle_times, output_filename):
-    fig, ax1 = plt.subplots()
-
-    max_cpus = len(clock_times) + 1
-    cpus = range(1, max_cpus)
-
-    ax1.set_xlabel('No. CPUs')
-    ax1.set_ylabel('Total Clock Time (s)')
-    ax1.plot(cpus, clock_times)
-
-    ax1.set_xlim(1, max_cpus)
-
-    ax2 = ax1.twinx()
-    ax2.set_ylabel('CPU Time Idle (%)')
-    ax2.plot(cpus, idle_times, color='r')
-
-    # get the output filename for this block
-    figures_directory = data.FiguresDirectory()
-    if not os.path.exists(figures_directory):
-        os.makedirs(figures_directory, exist_ok=True)
-
-    output_filename = '{}/{}'.format(figures_directory, output_filename)
-
-    # show the figure
-    plt.savefig(output_filename)
+    return np.max(cpu_times)
 
 
 
 def ComputeParallelStatistics(data, times):
-    # find the most cpus that could be useful (most jobs in one step)
-    maximum_cpus = 0
-    for step in times.keys():
-        maximum_cpus = max(maximum_cpus, len(times[step]))
+    nzblocks, nyblocks, nxblocks = data.NBlocks()
+    nblocks = nzblocks * nyblocks * nxblocks
 
-    total_clock_times = []
-    total_idle_times = []
+    # calculate the amount of time needed with a naive heuristic (FIFO)
+    for m_cpus in range(1, nblocks + 1):
+        print ('No. CPUs: {}'.format(m_cpus))
 
-    # calculate the optimal amount of time with this many CPUs
-    for cpu in range(1, maximum_cpus + 1):
-
-        # the clock time and idle time for this number of cpus
-        total_clock_time = 0
-        total_idle_time = 0
-
-        for step in times.keys():
-            # estimate the optimal time using the Longest Processing Time (LPT) heuristic
-            # achieves an upper bound of 4/3 - 1 / (3m) the optimal time
-            lpt_clock_time, lpt_idle_time = LPT(times[step], cpu)
-
-            # update the total clock and idle time after this step
-            total_clock_time += lpt_clock_time
-            total_idle_time += lpt_idle_time
-
-        total_clock_times.append(total_clock_time)
-        total_idle_times.append(total_idle_time)
-
-    PlotClockAndIdleTime(data, total_clock_times, total_idle_times, 'cpu-usage.png')
-
-
-
-def GenerateBlockTimingPlot(data, description, n_non_zero_voxels, times, output_filename):
-    fig, ax = plt.subplots()
-
-    # set the title and labels
-    ax.set_title('{} {}'.format(data.FigureTitleDescription(), description), fontsize=14)
-    ax.set_ylabel('CPU Time (s)', fontsize=10)
-    ax.set_xlabel('No. Non Zero Voxels (Millions)', fontsize=10)
-
-    # show a scatter plot of voxels and times
-    ax.scatter(n_non_zero_voxels, times)
-
-    # generate a best fit line
-    m, b = np.polyfit(n_non_zero_voxels, times, 1)
-
-    # create x and y chains for final plot
-    x = np.linspace(min(n_non_zero_voxels), max(n_non_zero_voxels), num=500)
-    y = m * x + b
-    ax.plot(x, y, color='#ff7d74', label='{:0.2f} seconds / million'.format(m))
-
-    # draw the legend
-    ax.legend()
-
-    fig.tight_layout()
-
-    # get the output filename for this block
-    figures_directory = data.FiguresDirectory()
-    if not os.path.exists(figures_directory):
-        os.makedirs(figures_directory, exist_ok=True)
-
-    output_filename = '{}/{}'.format(figures_directory, output_filename)
-
-    # show the figure
-    plt.savefig(output_filename)
-
-
-
-def ConductBlockTimingAnalysis(data, times):
-    # directory that contains
-    statistics_directory = '{}/statistics'.format(data.TempDirectory())
-
-    # create empty arrays for the block timing analysis
-    component_times = []
-    connect_labels_times = []
-    fill_holes_times = []
-    save_anchor_walls_times = []
-    compute_anchor_points_times = []
-    topological_thinning_times = []
-
-    # create aggregate variables
-    hole_filling_times = []
-    skeletonize_times = []
-    total_times = []
-
-    n_non_zero_voxels = []
-
-    # iterate over every block
-    index = 0 # counter to return the total time for each block
-    for iz in range(data.StartZ(), data.EndZ()):
-        for iy in range(data.StartY(), data.EndY()):
-            for ix in range(data.StartX(), data.EndX()):
-                statistics_filename = '{}/{:04d}z-{:04d}y-{:04d}x.pickle'.format(statistics_directory, iz, iy, ix)
-                statistics = ReadPickledData(statistics_filename)
-
-                # get the the number of non zero voxels in this block (in millions)
-                n_non_zero_voxels.append(statistics['filled_n_non_zero'] / million)
-
-                # get the times for each step for skeleton generation
-                save_anchor_walls_times.append(times['save-anchor-walls'][index])
-                compute_anchor_points_times.append(times['compute-anchor-points'][index])
-                topological_thinning_times.append(times['topological-thinning'][index])
-                skeletonize_times.append(save_anchor_walls_times[-1] + compute_anchor_points_times[-1] + topological_thinning_times[-1])
-
-                # get the time for each step for hole filling
-                if not data.HoleFillingOutputDirectory() == None:
-                    component_times.append(times['components'][index])
-                    connect_labels_times.append(times['connect-labels'][index])
-                    fill_holes_times.append(times['fill-holes'][index])
-                    hole_filling_times.append(component_times[-1] + connect_labels_times[-1] + fill_holes_times[-1])
-                    total_times.append(hole_filling_times[-1] + skeletonize_times[-1])
-
-                index += 1
-
-    GenerateBlockTimingPlot(data, 'Skeleton Generation Time', n_non_zero_voxels, skeletonize_times, 'skeleton-generation-time.png')
-
-    if not data.HoleFillingOutputDirectory() == None:
-        GenerateBlockTimingPlot(data, 'Hole Filling Time', n_non_zero_voxels, hole_filling_times, 'hole-filling-time.png')
-        GenerateBlockTimingPlot(data, 'Total Running Time', n_non_zero_voxels, total_times, 'total-running-time.png')
+        wall_time = WallTime(data, times, m_cpus, strategy = 'FIFO')
+        print ('  FIFO Strategy: {:0.2f} seconds'.format(wall_time))
+        wall_time = WallTime(data, times, m_cpus, strategy = 'LPT')
+        print ('  LPT Strategy: {:0.2f} seconds'.format(wall_time))
 
 
 
@@ -446,5 +496,7 @@ def ConductEndToEndTimingAnalysis(meta_filename):
     times['topological-thinning'] = TopologicalThinning(data)
     times['skeleton-refinement'] = SkeletonRefinement(data)
 
+
+
     ComputeParallelStatistics(data, times)
-    ConductBlockTimingAnalysis(data, times)
+    #ConductBlockTimingAnalysis(data, times)
